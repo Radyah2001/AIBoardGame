907ca728982465e452250d3cf7616de6ffe8cc23
arnarsnaeland <addisnae@gmail.com>
2023-03-10 14:00:11 +0100

    opening moves and piece-square tables

--- test.py
+++ test.py
@@ -3,8 +3,9 @@ import sys
 import pygame
 import chess
 import chess.engine
+import chess.polyglot
 
-MAX_DEPTH = 5
+MAX_DEPTH = 3
 
 # Initialize the Pygame module
 pygame.init()
@@ -27,6 +28,77 @@ engine = chess.engine.SimpleEngine.popen_uci(
 clock = pygame.time.Clock()
 fps = 60
 
+#Initialize piece square tables
+#Values taken from https://www.talkchess.com/forum3/viewtopic.php?f=2&t=68311&start=19
+#Position 0 in table is a8, position 63 is h1 (Seen from white's perspective)
+#Use directly for black, flip for white
+pawntable = [
+    0,   0,   0,   0,   0,   0,  0,   0,
+     98, 134,  61,  95,  68, 126, 34, -11,
+     -6,   7,  26,  31,  65,  56, 25, -20,
+    -14,  13,   6,  21,  23,  12, 17, -23,
+    -27,  -2,  -5,  12,  17,   6, 10, -25,
+    -26,  -4,  -4, -10,   3,   3, 33, -12,
+    -35,  -1, -20, -23, -15,  24, 38, -22,
+      0,   0,   0,   0,   0,   0,  0,   0,
+]
+
+knighttable = [
+    -167, -89, -34, -49,  61, -97, -15, -107,
+     -73, -41,  72,  36,  23,  62,   7,  -17,
+     -47,  60,  37,  65,  84, 129,  73,   44,
+      -9,  17,  19,  53,  37,  69,  18,   22,
+     -13,   4,  16,  13,  28,  19,  21,   -8,
+     -23,  -9,  12,  10,  19,  17,  25,  -16,
+     -29, -53, -12,  -3,  -1,  18, -14,  -19,
+    -105, -21, -58, -33, -17, -28, -19,  -23,
+    ]
+
+bishoptable = [
+    -29,   4, -82, -37, -25, -42,   7,  -8,
+    -26,  16, -18, -13,  30,  59,  18, -47,
+    -16,  37,  43,  40,  35,  50,  37,  -2,
+     -4,   5,  19,  50,  37,  37,   7,  -2,
+     -6,  13,  13,  26,  34,  12,  10,   4,
+      0,  15,  15,  15,  14,  27,  18,  10,
+      4,  15,  16,   0,   7,  21,  33,   1,
+    -33,  -3, -14, -21, -13, -12, -39, -21,
+    ]
+
+rooktable = [
+    32,  42,  32,  51, 63,  9,  31,  43,
+     27,  32,  58,  62, 80, 67,  26,  44,
+     -5,  19,  26,  36, 17, 45,  61,  16,
+    -24, -11,   7,  26, 24, 35,  -8, -20,
+    -36, -26, -12,  -1,  9, -7,   6, -23,
+    -45, -25, -16, -17,  3,  0,  -5, -33,
+    -44, -16, -20,  -9, -1, 11,  -6, -71,
+    -19, -13,   1,  17, 16,  7, -37, -26,
+]
+
+queentable = [
+    -28,   0,  29,  12,  59,  44,  43,  45,
+    -24, -39,  -5,   1, -16,  57,  28,  54,
+    -13, -17,   7,   8,  29,  56,  47,  57,
+    -27, -27, -16, -16,  -1,  17,  -2,   1,
+     -9, -26,  -9, -10,  -2,  -4,   3,  -3,
+    -14,   2, -11,  -2,  -5,   2,  14,   5,
+    -35,  -8,  11,   2,   8,  15,  -3,   1,
+     -1, -18,  -9,  10, -15, -25, -31, -50,
+]
+
+kingtable = [
+    -65,  23,  16, -15, -56, -34,   2,  13,
+     29,  -1, -20,  -7,  -8,  -4, -38, -29,
+     -9,  24,   2, -16, -20,   6,  22, -22,
+    -17, -20, -12, -27, -30, -25, -14, -36,
+    -49,  -1, -27, -39, -46, -44, -33, -51,
+    -14, -14, -22, -46, -44, -30, -15, -27,
+      1,   7,  -8, -64, -43, -16,   9,   8,
+    -15,  36,  12, -54,   8, -28,  24,  14,
+]
+
+psqt = [pawntable, knighttable, bishoptable, rooktable, queentable, kingtable]
 
 def board_evaluation(board):
     """
@@ -35,11 +107,11 @@ def board_evaluation(board):
     an advantage for Black.
     """
     piece_values = {
-        chess.PAWN: 1,
-        chess.KNIGHT: 3,
-        chess.BISHOP: 3,
-        chess.ROOK: 5,
-        chess.QUEEN: 9,
+        chess.PAWN: 100,
+        chess.KNIGHT: 300,
+        chess.BISHOP: 300,
+        chess.ROOK: 500,
+        chess.QUEEN: 900,
         chess.KING: 0  # The king's value is not used in this heuristic
     }
 
@@ -49,9 +121,9 @@ def board_evaluation(board):
         if piece is not None:
             value = piece_values[piece.piece_type]
             if piece.color == chess.WHITE:
-                score += value
+                score += value + psqt[piece.piece_type - 1][chess.square_mirror(square)]
             else:
-                score -= value
+                score -= value + psqt[piece.piece_type - 1][square]
 
     if piece is not None and piece.piece_type == chess.PAWN:
         if piece.color == chess.WHITE and chess.square_rank(square) == 6:
@@ -112,24 +184,28 @@ def minimax(board, depth, alpha, beta, maximizing_player):
 
 
 def get_best_move(board):
-    best_move = None
-    max_eval = float('-inf')
-    alpha = float('-inf')
-    beta = float('inf')
-    for move in board.legal_moves:
-        # Apply the move to the board
-        board.push(move)
-        # Evaluate the position after the move using the minimax algorithm with alpha-beta pruning
-        eval = minimax(board, MAX_DEPTH, alpha, beta, False)
-        # Undo the move
-        board.pop()
-        # Update the best move and maximum evaluation
-        if eval > max_eval:
-            max_eval = eval
-            best_move = move
-        if eval > alpha:
-            alpha = eval
-    return best_move
+    try:
+        move = chess.polyglot.MemoryMappedReader("human.bin").weighted_choice(board).move
+        return move
+    except:
+        best_move = None
+        max_eval = float('-inf')
+        alpha = float('-inf')
+        beta = float('inf')
+        for move in board.legal_moves:
+            # Apply the move to the board
+            board.push(move)
+            # Evaluate the position after the move using the minimax algorithm with alpha-beta pruning
+            eval = minimax(board, MAX_DEPTH, alpha, beta, False)
+            # Undo the move
+            board.pop()
+            # Update the best move and maximum evaluation
+            if eval > max_eval:
+                max_eval = eval
+                best_move = move
+            if eval > alpha:
+                alpha = eval
+        return best_move
 
 
 def draw_board(board, selected_square=None):
